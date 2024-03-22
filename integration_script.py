8e8ab4e217532992a0511a1d7ab819d100e40ef9
AbhijeetPatil161097 <157622300+AbhijeetPatil161097@users.noreply.github.com>
2024-03-22 18:12:47 +0530

    Update integration_script.py

--- integration_script.py
+++ integration_script.py
@@ -6,17 +6,18 @@
 
 import pandas as pd
 
-def merge_dataframes(df1, df2):
+def merge_dataframes(df1, df2, df3):
     # Rename columns
     df1.rename(columns={'SKU_NUMBER': 'VENDOR_ASSET_ID', 'MONTH_YEAR': 'TRANSACTION_DATE', 'NEW_TITLE': 'TITLE'}, inplace=True)
     df2.rename(columns={'VENDOR_IDENTIFIER': 'VENDOR_ASSET_ID', 'NEW_TITLE': 'TITLE', 'ROYALTY_PRICE': 'RETAIL_PRICE',
                         'CUSTOMER_PRICE': 'UNIT_COST', 'UNITS': 'QUANTITY', 'ASSET_CONTENT_FLAVOR': 'MEDIA_FORMAT',
                         'COUNTRY_CODE': 'TERRITORY', 'BEGIN_DATE': 'TRANSACTION_DATE'}, inplace=True)
-    #1
+    df3.rename(columns={'YOUTUBE_VIDEO_ID':'VENDOR_ASSET_ID', 'NEW_TITLE':'TITLE', 'RETAIL_PRICE_USD':'RETAIL_PRICE',
+                       'RESOLUTION':'MEDIA_FORMAT', 'COUNTRY':'TERRITORY'}, inplace=True)
+    
+    
+
     # Concatenate dataframes
-    df_combined = pd.concat([df1, df2], ignore_index=True)
-    df_combined['TRANSACTION_DATE'] = pd.to_datetime(df_combined['TRANSACTION_DATE'])
-    df_combined['TRANSACTION_DATE'] = df_combined['TRANSACTION_DATE'].dt.strftime('%B %Y')
-    return df_combined
-    #test1
+    final_df = pd.concat([df1, df2, df3], ignore_index=True)
+    return final_df
 
