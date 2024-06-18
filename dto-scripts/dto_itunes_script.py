6957da357f6db7fa707e8058ae3e94d0d8087e86
Abhijeet.Patil <157622300+AbhijeetPatil161097@users.noreply.github.com>
2024-06-18 19:22:20 +0530

    Update dto_itunes_script.py

--- dto-scripts/dto_itunes_script.py
+++ dto-scripts/dto_itunes_script.py
@@ -33,7 +33,7 @@ def read_data_from_s3_itunes(files_to_process, bucket_name):
                 file_name = os.path.basename(file_key)
                 file_extension = os.path.splitext(file_key)[1].lower()
 
-                file_name_month = extract_date_from_file_key(file_key, partner)
+                file_name_month = _extract_date_from_file_key(file_key, partner)
 
                 df = _read_file_from_s3(file_key, file_extension)
                 df = df.rename(columns=rename_mapping)
