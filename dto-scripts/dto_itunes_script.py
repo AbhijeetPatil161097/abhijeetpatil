693dd12f6d7e3432c3f3f7d0eff804bfa8b29f92
Abhijeet.Patil <157622300+AbhijeetPatil161097@users.noreply.github.com>
2024-06-18 19:17:28 +0530

    Update dto_itunes_script.py

--- dto-scripts/dto_itunes_script.py
+++ dto-scripts/dto_itunes_script.py
@@ -1,185 +1,269 @@
-#!/usr/bin/env python
-# coding: utf-8
+def read_data_from_s3_itunes(files_to_process, bucket_name):
+    try:
+        logging.info("Processing files for Itunes")
+        partner='itunes'
+        
+        '''
+        rename_mapping = {
+                'Start Date': 'Begin Date',
+                'ISRC/ISBN': 'ISRC',
+                'Quantity': 'Units',
+                'Partner Share': 'Royalty Price',
+                'Partner Share Currency': 'Royalty Currency',
+                'Artist/Show/Developer/Author': 'Artist / Show',
+                'Label/Studio/Network/Developer/Publisher': 'Label/Studio/Network',
+                'ISAN/Other Identifier': 'ISAN',
+                'Country Of Sale': 'Country Code',
+                'Pre-order Flag': 'PreOrder'
+            }
+        '''
+        df_list_itunes = []
 
-# In[3]:
+        itunes_files_df = _filter_partner_files(files_to_process, partner)
+        if itunes_files_df.empty:
+            logging.error("No new Itunes files to process.")
+            return pd.DataFrame()
+
+        for index, row in itunes_files_df.iterrows():
+            try:
+                s3_url = row['raw_file_path']
+                file_key = s3_url.split(f's3://{bucket_name}/')[1]
+                logging.info(f"Processing started for file: {file_key}")
+
+                file_name = os.path.basename(file_key)
+                file_extension = os.path.splitext(file_key)[1].lower()
+
+                file_name_month = extract_date_from_file_key(file_key, partner)
+
+                df = _read_file_from_s3(file_key, file_extension)
+                df = df.rename(columns=rename_mapping)
+
+                if df is None:
+                    logging.error(f"Itunes file is empty or could not be read: {file_key}")
+                    continue
+
+                df['Start Date'] = pd.to_datetime(df['Start Date'], format = '%m/%d/%Y')
+                df['End Date'] = pd.to_datetime(df['End Date'], format = '%m/%d/%Y')
+
+                # Raise error if difference between Start Date and End Date is more  than 45 Days.
+                date_diff = (df['End Date'] - df['Start Date']).dt.days
+                if (date_diff > 45).any():
+                    logging.error("Begin Date and End Date difference is more than 1 month.")
 
 
-import numpy as np
-import pandas as pd
+                unique_months = ','.join(df['Start Date'].unique())
+                file_info = s3.info(f"{bucket_name}/{file_key}")
+                file_creation_date = file_info['LastModified'].strftime('%Y-%m-%d')
+                file_row_count = len(df)
+                metrics= ['QUANTITY (Quantity)', 'REVENUE_NATIVE (Extended Partner Share)']
+                raw_file_values = [df['Quantity'].astype('int').sum(), 
+                                   df['Extended Partner Share'].astype('float').sum()
+                                  ]
+
+                _collect_file_metadata(bucket_name, 
+                                       file_key, 
+                                       file_name, 
+                                       file_creation_date,
+                                       file_name_month,
+                                       partner,
+                                       unique_months,
+                                       file_row_count
+                                      )
+
+                _collect_metric_metadata(bucket_name,
+                                         file_key, 
+                                         file_name, 
+                                         partner,
+                                         unique_months, metrics= metrics, 
+                                         raw_file_values = raw_file_values
+                                        )
+
+                df_list_itunes.append(df)
+                logging.info(f"Processing completed for file: {file_key}")
+
+            except Exception as e:
+                logging.error(f"An error occurred while reading Itunes data: {e}")
+
+        df_itunes = pd.concat(df_list_itunes, ignore_index=True)
+        raw_metadata = pd.DataFrame(new_raw_metadata)
+        df_itunes_filtered = _remove_associated_files(df_itunes, itunes_files_df, raw_metadata)
+        
+        '''
+        column_name = ['Asset/Content Flavor', 'Primary Genre', 'Provider Country', 'Sales or Return']
+        for column in column_name:
+            if column not in df_itunes_filtered.columns:
+                df_itunes_filtered[column] = None
+        '''
+
+        logging.info("Amazon DataFrame created successfully")
+        return df_itunes_filtered
+        
+    except Exception as e:
+        logging.error(f"An error occurred while reading Amazon data: {e}")
+        return pd.DataFrame()
+
 
 class DtoDataProcessItunes:
-
-    # Column name variables
-    PROVIDER = 'Provider'
-    PROVIDER_COUNTRY = 'Provider Country'
-    VENDOR_IDENTIFIER = 'Vendor Identifier'
-    UPC = 'UPC'
-    ISRC = 'ISRC'
-    ARTIST_SHOW = 'Artist / Show'
-    TITLE = 'Title'
-    LABEL_STUDIO_NETWORK = 'Label/Studio/Network'
-    PRODUCT_TYPE_IDENTIFIER = 'Product Type Identifier'
-    UNITS = 'Units'
-    ROYALTY_PRICE = 'Royalty Price'
-    BEGIN_DATE = 'Begin Date'
-    END_DATE = 'End Date'
-    CUSTOMER_CURRENCY = 'Customer Currency'
-    COUNTRY_CODE = 'Country Code'
-    ROYALTY_CURRENCY = 'Royalty Currency'
-    PREORDER = 'PreOrder'
-    ISAN = 'ISAN'
-    APPLE_IDENTIFIER = 'Apple Identifier'
-    CUSTOMER_PRICE = 'Customer Price'
-    CMA = 'CMA'
-    ASSET_CONTENT_FLAVOR = 'Asset/Content Flavor'
-    VENDOR_OFFER_CODE = 'Vendor Offer Code'
-    GRID = 'Grid'
-    PROMO_CODE = 'Promo Code'
-    PARENT_IDENTIFIER = 'Parent Identifier'
-    PARENT_TYPE_ID = 'Parent Type Id'
-    PRIMARY_GENRE = 'Primary Genre'
-    EXTENDED_PARTNER_SHARE = 'Extended Partner Share'
-    SALES_OR_RETURN = 'Sales or Return'
-    REGION = 'Region'
-    REVENUE_NATIVE = 'REVENUE_NATIVE'
-    COST_NATIVE = 'COST_NATIVE'
-
-    
-    
     def __init__(self, platform, df):
         self.platform = platform
-        self.df = df
-        self.columns_to_drop = [self.PROVIDER, self.UPC, self.ISRC, self.LABEL_STUDIO_NETWORK, 
-                                self.PRODUCT_TYPE_IDENTIFIER, self.CUSTOMER_CURRENCY, self.PREORDER, 
-                                self.ISAN, self.APPLE_IDENTIFIER, self.CMA, self.VENDOR_OFFER_CODE, self.GRID, self.PROMO_CODE,
-                                self.PARENT_IDENTIFIER, self.PARENT_TYPE_ID, self.EXTENDED_PARTNER_SHARE, self.REGION, self.END_DATE,
-                                self.PROVIDER_COUNTRY]
+        self.df = df.copy()
         
-        self.title_columns = [self.ARTIST_SHOW, self.TITLE]
-        self.metric_columns = [self.CUSTOMER_PRICE, self.ROYALTY_PRICE, self.UNITS]
-        self.df.dropna(subset=[self.VENDOR_IDENTIFIER, self.TITLE, self.COUNTRY_CODE], inplace=True)
-        self.groupby_columns = [self.VENDOR_IDENTIFIER, self.COUNTRY_CODE, 'NEW_TITLE', self.BEGIN_DATE]
-        self.currency_to_country_mapping = {'USD': 'US', 'CAD': 'CA', 'EUR': 'DE', 'GBP': 'GB', 'AUD': 'AU'}
+        '''                            
+        # As the raw data is generated by partner and output should be from the perspective of A+E, we are renaming
+          the mteric columns.
+        Royalty Price is what A+E gets from partner so it becomes Unit Revenue Native for A+E.
         
-        self.sales_return_mapping = {'S': 'SALES', 'R': 'RETURN'}
+        '''
+        self.column_rename_map = {
+            'Customer Price' : 'Unit Retail Price Native',
+            'Partner Share' : 'Unit Revenue Native',
+            'Extended Partner Share' : 'Revenue Native'
+        }
         
-        self.df[self.BEGIN_DATE] = pd.to_datetime(self.df[self.BEGIN_DATE]).dt.strftime('%Y-%m')
-        self.df[self.ASSET_CONTENT_FLAVOR].fillna('UNKNOWN', inplace=True)
-        self.df[self.PRIMARY_GENRE].fillna('UNKNOWN', inplace=True)
+        self.df.rename(columns=self.column_rename_map, inplace=True)
+ 
+        self.columns_to_drop = [
+            'Provider', 'UPC', 'ISRC', 'Label/Studio/Network', 
+            'Product Type Identifier', 'Customer Currency', 'PreOrder', 
+            'ISAN', 'Apple Identifier', 'CMA', 'Vendor Offer Code', 'Grid', 'Promo Code',
+            'Parent Identifier', 'Parent Type Id', 'Extended Partner Share', 'Region', 'End Date',
+            'Provider Country'
+        ]
+        self.title_columns = ['Artist/Show/Developer/Author', 'Title']
+        self.new_title_col = 'NEW_TITLE'
+        self.new_partner_col = 'PARTNER_TITLE'
+        
+        self.unit_retail_price = 'Unit Retail Price Native'
+        self.unit_revenue_col = 'Unit Revenue Native'
+        self.revenue_col = 'Revenue Native'
+        self.quantity_col = 'Quantity'
+        self.retail_price = 'Retail Price Native'
+        self.territory_col = 'Country Of Sale'
+        self.sku_col = 'Vendor Identifier'
+        self.groupby_columns = ['Vendor Identifier', 'Country Of Sale', 'NEW_TITLE', 'Start Date']
+        self.agg_columns = {
+            'Unit Retail Price Native': 'mean', 
+            'Unit Revenue Native': 'mean', 
+            'Quantity': 'sum', 
+            'Revenue Native': 'sum', 
+            'Retail Price Native': 'sum', 
+            #'ASSET/CONTENT FLAVOR': lambda x: '|'.join(sorted(pd.Series.unique(x))),
+            'PARTNER_TITLE': lambda x: '%%'.join(pd.Series.unique(x))
+        }
 
         
         
-        # Drop redundant columns
-    def drop_columns(self):
+    def drop_columns(self, columns_to_drop):
         try:
-            self.df.drop(columns=[col for col in self.columns_to_drop if col in self.df.columns], inplace=True)
+            existing_columns = [col for col in columns_to_drop if col in self.df.columns]
+            self.df.drop(columns=existing_columns, inplace=True)
         except Exception as e:
             raise RuntimeError(f"Error dropping columns: {e}")
-
-    # Create NEW_TITLE column by concatenating title columns
-    def new_title(self, row):
+    
+    def new_title(self, row, title_columns):
         try:
-            season_title = str(row[self.ARTIST_SHOW]).replace('"', '').strip()
-            title = str(row[self.TITLE]).replace('"', '').strip()
+            series_title = str(row[title_columns[0]]).replace('"', '').strip()
+            title = str(row[title_columns[1]]).replace('"', '').strip()
 
-            if season_title in title:
-                season_title_part = season_title[len(season_title):].strip()
-                return f"{season_title_part} | {title}"
+            if series_title in title:
+                series_title_part = series_title[len(series_title):].strip()
+                return f"{series_title_part} | {title}"
             else:
-                return f"{season_title} | {title}"
-        except Exception as e:
-            raise RuntimeError(f"Error processing new title: {e}")
+                return f"{series_title} | {title}"
+        except KeyError as e:
+            raise RuntimeError(f"Error processing title: {e}")
+    
+    
+    def new_partner_title(self, row, title_columns):
+        try:
+            titles = [str(row[col]).replace('"', '').strip() for col in title_columns]
+            return ' || '.join(titles)
+        except KeyError as e:
+            raise RuntimeError(f"Error processing title: {e}")
 
-    # Add NEW_TITLE to df and delete old title columns
-    def process_new_title_and_drop_columns(self):
+    def process_new_title_and_drop_columns(self, title_columns, new_title_col, new_partner_col):
         try:
-            self.df['NEW_TITLE'] = self.df.apply(self.new_title, axis=1)
-            self.df['NEW_TITLE'] = self.df['NEW_TITLE'].fillna('').apply(lambda x: x.replace('|', '').strip())
-            for column in self.title_columns:
-                if column in self.df.columns:
-                    self.df = self.df.drop(column, axis=1)
-        except Exception as e:
-            raise RuntimeError(f"Error processing title and dropping columns: {e}")
+            self.df[new_title_col] = self.df.apply(lambda row: self.new_title(row, title_columns), axis=1)
+            self.df[new_partner_col] = self.df.apply(lambda row: self.new_partner_title(row, title_columns), axis=1)
+            self.df[new_title_col] = self.df[new_title_col].fillna('').apply(lambda x: x.replace('|', '').strip())
+            self.df.drop(columns=title_columns, inplace=True)
+        except KeyError as e:
+            raise RuntimeError(f"Error dropping old title columns: {e}")
 
-    # Map Missing country codes using currency
-    def map_country_codes(self):
-        try:
-            self.df[self.COUNTRY_CODE] = self.df[self.COUNTRY_CODE].str.strip().fillna(
-                self.df[self.ROYALTY_CURRENCY].map(self.currency_to_country_mapping)
-            )
-            self.df.drop(columns=[self.ROYALTY_CURRENCY], inplace=True)
-        except Exception as e:
-            raise RuntimeError(f"Error mapping country codes: {e}")
-            
-    # Calculate revenue and cost       
-    def calculate_revenue_cost(self):
-        try:
-            self.df['REVENUE_NATIVE'] = self.df[self.CUSTOMER_PRICE] * self.df[self.UNITS]
-            self.df['COST_NATIVE'] = self.df[self.ROYALTY_PRICE] * self.df[self.UNITS] 
-        except Exception as e:
-            raise RuntimeError(f"Error calculating revenue and cost: {e}")
 
             
-    # Rename S =SALES, R=RETURN 
-    def map_sales_return(self):
+    def replace_titles(self, territory_col, sku_col, new_title_col):
         try:
-            self.df[self.SALES_OR_RETURN] = self.df[self.SALES_OR_RETURN].map(self.sales_return_mapping)
-            self.df[self.SALES_OR_RETURN].fillna('UNKNOWN', inplace=True)
-        except Exception as e:
-            raise RuntimeError(f"Error mapping sales/return: {e}")
+            mask = (self.df[territory_col] == 'US') & (self.df[sku_col] != np.nan)
+            filtered_data = self.df[mask]
 
-    # Aggregate rows    
-    def aggregate_data(self):
+            grouped_titles = filtered_data.groupby(sku_col)[new_title_col]
+            most_frequent_titles = grouped_titles.apply(lambda x: x.mode()[0] if not x.empty else x.iloc[0]).reset_index(name=new_title_col)
+
+            merged_data = self.df.merge(most_frequent_titles, on=sku_col, how='left', suffixes=('_x', '_y'))
+            merged_data[new_title_col] = merged_data.pop(new_title_col + '_y').combine_first(merged_data.pop(new_title_col + '_x')).str.strip()
+
+            self.df = merged_data
+            return self.df
+        except KeyError as e:
+            raise KeyError(f"Error replacing titles - KeyError: {e}")
+    
+    def calculate_metric_values(self, unit_retail_price, retail_price, quantity_col, revenue_col, unit_revenue_col):
         try:
-            def unique_join(series):
-                return '|'.join(sorted(pd.Series.unique(series)))
-
-            agg_columns = {
-                self.CUSTOMER_PRICE: 'mean',
-                self.ROYALTY_PRICE: 'mean',
-                self.UNITS: 'sum',
-                self.REVENUE_NATIVE: 'sum',
-                self.COST_NATIVE: 'sum',
-                self.SALES_OR_RETURN: unique_join,
-                self.PRIMARY_GENRE: unique_join,
-                self.ASSET_CONTENT_FLAVOR: unique_join
-            }
-
-            self.df = self.df.groupby(self.groupby_columns).agg(agg_columns).reset_index()
+            self.df[revenue_col] = self.df[unit_revenue_col] * self.df[quantity_col].abs()
+            self.df[retail_price] = self.df[unit_retail_price] * self.df[quantity_col].abs()
+            
         except Exception as e:
+            raise RuntimeError(f"Error calculating revenue: {e}")
+            
+    def aggregate_data(self, groupby_columns, agg_columns):
+        try:
+            self.df = self.df.groupby(groupby_columns).agg(agg_columns).reset_index()
+        except KeyError as e:
             raise RuntimeError(f"Error aggregating data: {e}")
-
-
-    # Format column names       
+            
+    def calculate_weigted_mean(self, unit_retail_price, retail_price, quantity_col, revenue_col, unit_revenue_col):
+        try:
+            self.df[unit_retail_price] = self.df[retail_price] / self.df[quantity_col].abs()
+            self.df[unit_revenue_col] = self.df[revenue_col] / self.df[quantity_col].abs()
+        except Exception as e:
+            raise RuntimeError(f"Error calculating weighted mean: {e}")
+    
     def rename_columns(self):
         try:
-            self.df = self.df.rename(columns=lambda x: x.upper().replace(' ', '_'))
+            self.df.rename(columns=lambda x: x.upper().replace(' ', '_'), inplace=True)
         except Exception as e:
             raise RuntimeError(f"Error renaming columns: {e}")
-
-    # Add ITUNES as vendor name
-    def add_vendor_name_column(self, vendor_name='iTUNES'):
+    
+    def add_columns_to_df(self, vendor_name):
         try:
-            self.df.insert(0, 'VENDOR_NAME', vendor_name)
+            self.df.insert(0, 'PARTNER', vendor_name)
+            self.df['REVENUE_USD'] = np.nan
+            self.df['IS_CONVERSION_RATE'] = False
             return self.df
         except Exception as e:
             raise RuntimeError(f"Error adding vendor name column: {e}")
 
-    # Create a function to call all above functions
     def process_data_source(self):
         try:
-            self.drop_columns()
-            self.process_new_title_and_drop_columns()
-            self.map_country_codes()
-            self.map_sales_return()
-            self.calculate_revenue_cost()
-            self.aggregate_data()
+            self.drop_columns(self.columns_to_drop)
+            self.process_new_title_and_drop_columns(self.title_columns, self.new_title_col, self.new_partner_col)
+            self.replace_titles(self.territory_col, self.sku_col, self.new_title_col)
+            
+            self.calculate_metric_values(self.unit_retail_price, 
+                                         self.retail_price, 
+                                         self.quantity_col, 
+                                         self.revenue_col, 
+                                         self.unit_revenue_col
+                                        )
+            self.calculate_weigted_mean(self.unit_retail_price, 
+                                         self.retail_price, 
+                                         self.quantity_col, 
+                                         self.revenue_col, 
+                                         self.unit_revenue_col
+                                        )
+            self.aggregate_data(self.groupby_columns, self.agg_columns)
             self.rename_columns()
-            return self.add_vendor_name_column()
+            return self.add_columns_to_df('itunes')
         except RuntimeError as e:
             raise e
-# In[ ]:
-
-
-
-
