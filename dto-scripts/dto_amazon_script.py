e731d7a0b707a15b9bf545f97b5bba284b300793
Abhijeet.Patil <157622300+AbhijeetPatil161097@users.noreply.github.com>
2024-06-18 17:22:50 +0530

    Update dto_amazon_script.py

--- dto-scripts/dto_amazon_script.py
+++ dto-scripts/dto_amazon_script.py
@@ -37,7 +37,7 @@ def read_data_from_s3_amazon(files_to_process, bucket_name):
 
             file_name = os.path.basename(file_key)
             file_extension = os.path.splitext(file_key)[1].lower()
-            df = _read_file_from_s3(file_key, file_extension)
+            df = _read_file_from_s3(bucket_name, file_key, file_extension)
 
             if df is None:
                 logging.error(f"Amazon file is empty or could not be read: {file_key}")
